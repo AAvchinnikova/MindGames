d2adcda068808439ca223b6ea3573fe6e4ea3ea7
AAvchinnikova <avchinnikova_a@mail.ru>
2024-03-31 07:29:07 +0200

    step 4

--- app/src/main/java/hexlet/code/GameOne.java
+++ app/src/main/java/hexlet/code/GameOne.java
@@ -1,13 +1,15 @@
 package hexlet.code;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameOne {
     public static void gameOne() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Your choice: 1\n Welcome to the Brain Games!\n May I have your name?");
         String userName = scanner.next();
-        var number = Math.random();
-        System.out.println("Hello, " + userName + "!" +"\nAnswer 'yes' if the number is even, otherwise answer 'no'/n" + number);
+        var number = ThreadLocalRandom.current().nextInt(1000);
+        System.out.println("Hello, " + userName + "!" +"Answer 'yes' if the number is even, otherwise answer 'no'");
+        System.out.println("Question: " + number);
         var answerPlayer = scanner.next();
         if (number % 2 == 0 & answerPlayer.equals("yes")) {
             System.out.println("Correct!");
