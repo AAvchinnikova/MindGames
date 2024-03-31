8ba3e110c166e961eeb7bf15beedd4c5d29931bb
AAvchinnikova <avchinnikova_a@mail.ru>
2024-03-31 06:57:36 +0200

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 0 - Exit");
         Cli.hello();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 public class Cli {
     public static void hello() {
         Scanner scanner = new Scanner(System.in);
+        var numberGame = scanner.next();
 
-        System.out.println("May I have your name?");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-
+        if (numberGame.equals("1")) {
+            System.out.println("Welcome to the Brain Games! Your choice: 1 May I have your name?");
+            String userName = scanner.next();
+            System.out.println("Hello, " + userName + "!");
+        } else if (numberGame.equals("0")) {
+            System.out.println("Exit");
+        }
         scanner.close();
     }
 }
