91ae06b7d6cf4d3014b83239033135cab4610532
AAvchinnikova <avchinnikova_a@mail.ru>
2024-04-01 08:23:14 +0200

    games 1

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -10,20 +10,20 @@ public class Engine {
         System.out.println("May I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName);
-        switch (Integer.parseInt(numberGame)) {
-            case 1:
-                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'");
-                String number = "";
-                String answer = "";
-                for (var i = 1; i <= 3; i++){
-                    if (!number.equals("wrong")) {
-                        String result = GameOne.gameOne(answer, number);
-                        System.out.println("Correct!");
-                        System.out.println("Quation: " + result);
-                        answer = scanner.next();
-                        number = result;
-                    } System.out.println("Wrong game1");
-                } break;
+        String resultForUser = "";
+        String answearUser = "";
+        String answearGame = "";
+        for (var i = 1; i < 3; i++) {
+            switch (Integer.parseInt(numberGame)) {
+                case 1:
+                    resultForUser = GameOne.gameOne(answearUser,resultForUser);
+                    break;
+                case 2:
+                    answearGame = "";
+                    break;
+            }
+            System.out.println ("Question:" + resultForUser);
+            answearUser = scanner.next();
         }
     }
 }
--- app/src/main/java/hexlet/code/GameOne.java
+++ app/src/main/java/hexlet/code/GameOne.java
@@ -3,16 +3,22 @@ package hexlet.code;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GameOne {
-    public static String gameOne(String answear, String numberGame) {
-        var numberToInt = ThreadLocalRandom.current().nextInt(100);
+    public static String gameOne(String answear, String resultForUser) {
+        var number = ThreadLocalRandom.current().nextInt(1000);
 
-        if (answear.equals("") & numberGame .equals("")) {
-            return Integer.toString(numberToInt);
-        }
-        if (Integer.parseInt(numberGame) % 2 == 0 & answear.equals("yes")){
-            return Integer.toString(numberToInt);
-            } else if (Integer.parseInt(numberGame) % 2 != 0 & answear.equals("no")) {
-            return Integer.toString(numberToInt);
-            } return "wrong";
+        if (answear.isEmpty() & resultForUser.isEmpty()) {
+            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'");
+            answear = Integer.toString(number);
+        } else {
+            if (Integer.parseInt(resultForUser) % 2 == 0 & answear.equals("yes")) {
+                System.out.println("Correct!");
+                answear = Integer.toString(number);
+            } else if (Integer.parseInt(resultForUser) % 2 != 0 & answear.equals("no")) {
+                System.out.println("Correct!");
+                answear = Integer.toString(number);
+            } else {
+                answear = "Wrong";
+            }
+        } return answear;
     }
 }
