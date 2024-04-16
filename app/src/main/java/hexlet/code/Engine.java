70db5c107c597d65d7160575e4864a48f862d6de
AAvchinnikova <avchinnikova_a@mail.ru>
2024-04-16 09:08:27 +0200

    Stage 2

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,14 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    private static String userName;
+    private static final String USERNAME;
     public static final int STEPS = 3;
     static {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nWelcome to the Brain Games!");
         System.out.println("May I have your name?");
-        userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+        USERNAME = scanner.next();
+        System.out.println("Hello, " + USERNAME + "!");
     }
     public static void meetWithUser(String questionForUser) {
         System.out.println(questionForUser);
@@ -24,9 +24,10 @@ public class Engine {
     }
     public static void finishGame(String questionForUser) {
         if (questionForUser.equals("Wrong")) {
-            System.out.println("Let's try again, " + userName + "!");
+            System.out.println("Let's try again, " + USERNAME + "!");
         } else {
-            System.out.println("Congratulations, " + userName + "!");
+            System.out.println("Congratulations, " + USERNAME + "!");
         }
     }
 }
+
